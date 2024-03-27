7fc9c605e5cb47ba6e139df1eeda58b178846d9a
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2024-03-27 18:19:23 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+const dog = {
+    name: "Mango",
+    age: 3,
+    isHappy: true,
+  };
+  
+  const dogJSON = JSON.stringify(dog);
+  console.log(dogJSON);
+  
