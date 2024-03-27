10a6c80b852e23b4caf447f557cf93e645e0388f
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2024-03-27 16:20:05 +0000

    Deploying to gh-pages from @ IgorShamborskiy/IgorBoraJS@7fc9c605e5cb47ba6e139df1eeda58b178846d9a 🚀

---
+++ 01-gallery.5c497cb8.js
@@ -0,0 +1,2 @@
+console.log(JSON.stringify({name:"Mango",age:3,isHappy:!0}));
+//# sourceMappingURL=01-gallery.5c497cb8.js.map
---
+++ 01-gallery.f8874118.js
@@ -0,0 +1,2 @@
+console.log(JSON.stringify({name:"Mango",age:3,isHappy:!0}));
+//# sourceMappingURL=01-gallery.f8874118.js.map
